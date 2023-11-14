7a471b71c6403fd77d43a9023aea1241c1a7f3f8
ylberuksmajli <ukzmaili01@gmail.com>
2023-11-14 22:33:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Define a route for the home page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'carshop.html'));
 });
 
 // Start the server
